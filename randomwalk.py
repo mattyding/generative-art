b619a922ce38cf43a5742f60397c42d5ba7a4aff
mattyding <66889540+mattyding@users.noreply.github.com>
2022-08-06 11:55:11 -0700

    compression

--- randomwalk.py
+++ randomwalk.py
@@ -6,27 +6,38 @@ Date: 08/06/2022
 
 This file contains code for generating animations of random 2D walks.
 """
+import os
+from signal import pause
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from datetime import datetime
+
+FRAME_SKIP = 4  # for reducing the size of the file
+PAUSE_RATIO = 0.6
 
 def random_unit_vector():
     angle = np.random.random() * 2 * np.pi
     return np.cos(angle), np.sin(angle)
 
 def random_walk(num_steps, max_step):
-    x, y = np.random.random(2) * 2 - 1  # random point between -1 and 1
+    x, y = np.random.random(2) * 2 - 1  # random points between -1 and 1
     steps = [(x, y)]
-    for _ in range(num_steps):
+    for _ in range(num_steps * FRAME_SKIP):
         step_size = np.random.random() * max_step
-        x += random_unit_vector()[0] * step_size
-        y += random_unit_vector()[1] * step_size
+        ang_x, ang_y = random_unit_vector()
+        x += ang_x * step_size
+        y += ang_y * step_size
         steps.append((x,y))
     return np.array((steps))
 
-def update_lines(num_steps, walks, lines):
+
+def update_lines(num_steps, walks, lines, pause_threshold):
     for walk, line in zip(walks, lines):
-        line.set_data(walk[:num_steps, 0], walk[:num_steps, 1])
+        if num_steps < pause_threshold:
+            line.set_data(walk[:num_steps*FRAME_SKIP, 0], walk[:num_steps*FRAME_SKIP, 1])
+        else:  # pause animation at end
+            line.set_data(walk[:pause_threshold*FRAME_SKIP, 0], walk[:pause_threshold*FRAME_SKIP, 1])
     return lines
 
 # https://stackoverflow.com/questions/14720331/how-to-generate-random-colors-in-matplotlib
@@ -35,22 +46,30 @@ def get_cmap(n, name='hsv'):
     RGB color; the keyword argument name must be a standard mpl colormap name.'''
     return plt.cm.get_cmap(name, n)
 
-def random_walk_animation(num_steps, walks, cmap, seed):
+
+def random_walk_animation(params, walks, cmap, seed):
+    num_steps, max_step_size, num_walks = params
+    pause_threshold = int(num_steps * PAUSE_RATIO)
+
     fig, ax = plt.subplots()
     lines = [ax.plot(walk[0], walk[1], color=cmap(i))[0] for i, walk in enumerate(walks)]
     plt.xlim(-1, 1)
     plt.ylim(-1, 1)
     plt.axis('off')
-    anim = animation.FuncAnimation(fig, update_lines, fargs=(walks, lines),
+    anim = animation.FuncAnimation(fig, update_lines, fargs=(walks, lines, pause_threshold),
                                    frames=num_steps, interval=100)
     #plt.show()
-    plt.savefig(f"./figures/randomwalk/walk-{seed}.png")
-    writergif = animation.PillowWriter(fps=30) 
-    anim.save(f"./figures/randomwalk/walk-{seed}.gif" , writer=writergif)
-    return fig, anim
+    plt.savefig(f"./figures/randomwalk/{seed}-{num_steps}-{max_step_size}-{num_walks}/walk-full.png", dpi=500)
+    writergif = animation.PillowWriter(fps=20) 
+    anim.save(f"./figures/randomwalk/{seed}-{num_steps}-{max_step_size}-{num_walks}/walk.gif" , writer=writergif)
+
+    fig.savefig(f"./figures/randomwalk/{seed}-{num_steps}-{max_step_size}-{num_walks}/walk-small.png", dpi=500)
 
 
-def random_walk_stats_animation(num_steps, walks, cmap, seed):
+def random_walk_stats_animation(params, walks, cmap, seed):
+    num_steps, max_step_size, num_walks = params
+    pause_threshold = int(num_steps * PAUSE_RATIO)
+
     fig, _ = plt.subplots()
     fig.set_figheight(5)
     fig.set_figwidth(10)
@@ -75,44 +94,52 @@ def random_walk_stats_animation(num_steps, walks, cmap, seed):
 
     variance_plot = plt.subplot2grid((2, 4), (1, 3), rowspan=1, colspan=2)
 
-    def update_graph(num_steps, walks, lines):
-        for walk, line in zip(walks, lines):
-            line.set_data(walk[:num_steps, 0], walk[:num_steps, 1])
-        
-        expectation_line.set_data(expectations_x[:num_steps], expectations_y[:num_steps])
-        if num_steps > 1:
-            # 0.1 buffer from edges
-            expectation_plot.set_xlim(min(expectations_x[:num_steps])-0.1, max(expectations_x[:num_steps])+0.1)
-            expectation_plot.set_ylim(min(expectations_y[:num_steps])-0.1, max(expectations_y[:num_steps])+0.1)
+    def update_graph(num_steps, walks, lines, pause_threshold):
+        lines = update_lines(num_steps, walks, lines, pause_threshold)
+
         variance_plot.clear()
-        variance_plot.plot(variances[:num_steps], color='black')
         variance_plot.title.set_text('Variance')
         variance_plot.set_xlabel('Step')
+
+        if num_steps < pause_threshold:
+            expectation_line.set_data(expectations_x[:num_steps*FRAME_SKIP], expectations_y[:num_steps*FRAME_SKIP])
+            if num_steps > 1:
+                # 0.1 buffer from edges
+                expectation_plot.set_xlim(min(expectations_x[:num_steps*FRAME_SKIP])-0.1, max(expectations_x[:num_steps*FRAME_SKIP])+0.1)
+                expectation_plot.set_ylim(min(expectations_y[:num_steps*FRAME_SKIP])-0.1, max(expectations_y[:num_steps*FRAME_SKIP])+0.1)
+                variance_plot.plot(variances[:num_steps*FRAME_SKIP], color='black')
+        else:
+            expectation_line.set_data(expectations_x[:pause_threshold*FRAME_SKIP], expectations_y[:pause_threshold*FRAME_SKIP])
+            if num_steps > 1:
+                # 0.1 buffer from edges
+                expectation_plot.set_xlim(min(expectations_x[:pause_threshold*FRAME_SKIP])-0.1, max(expectations_x[:pause_threshold*FRAME_SKIP])+0.1)
+                expectation_plot.set_ylim(min(expectations_y[:pause_threshold*FRAME_SKIP])-0.1, max(expectations_y[:pause_threshold*FRAME_SKIP])+0.1)
+                variance_plot.plot(variances[:pause_threshold*FRAME_SKIP], color='black')
         return lines
 
-    anim = animation.FuncAnimation(fig, update_graph, fargs=(walks, lines),
+    anim = animation.FuncAnimation(fig, update_graph, fargs=(walks, lines, pause_threshold),
                                    frames=num_steps, interval=100)
     #plt.show()
-    writergif = animation.PillowWriter(fps=30) 
-    anim.save(f"./figures/randomwalk/stats-{seed}.gif" , writer=writergif)
-    return anim
+    writergif = animation.PillowWriter(fps=20) 
+    anim.save(f"./figures/randomwalk/{seed}-{num_steps}-{max_step_size}-{num_walks}/stats.gif" , writer=writergif)
 
 
-def random_walk_art(num_steps=400, max_steps=0.15, num_walks=50):
+def random_walk_art(num_steps=400, max_step_size=0.15, num_walks=50):
     seed =  np.random.randint(0, 1000000)
     np.random.seed(seed)
-    walks = [random_walk(num_steps, max_steps) for _ in range(num_walks)]
+    os.mkdir(f"{os.getcwd()}/figures/randomwalk/{seed}-{num_steps}-{max_step_size}-{num_walks}")
+    walks = [random_walk(num_steps, max_step_size) for _ in range(num_walks)]
     cmap = get_cmap(num_walks)
-    random_walk_animation(num_steps, walks, cmap, seed)
-    random_walk_stats_animation(num_steps, walks, cmap, seed)
+    random_walk_animation((num_steps, max_step_size, num_walks), walks, cmap, seed)
+    random_walk_stats_animation((num_steps, max_step_size, num_walks), walks, cmap, seed)
 
 
 def main():
-    for _ in range(10):
-        num_steps = np.random.randint(200, 500)
-        max_steps = np.random.uniform(0.01, 0.3)
+    for _ in range(6):
+        num_steps = np.random.randint(100, 150)
+        max_step_size = np.random.uniform(0.01, 0.3)
         num_walks = np.random.randint(20, 100)
-        random_walk_art(num_steps, max_steps, num_walks)
+        random_walk_art(num_steps, round(max_step_size, 2), num_walks)
 
 
 if __name__ == '__main__':
